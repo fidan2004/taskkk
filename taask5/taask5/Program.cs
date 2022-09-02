1ad56e7453ee61d84525c82cbfb13f5db995ba5d
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-09-02 21:39:12 +0400

    Add files via upload

---
+++ taask5/taask5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace taask5
+{
+   public class Program
+    {
+        static void Main(string[] args)
+           {
+                Dictionary<string, bool> characters = new Dictionary<string, bool>()
+                {
+
+                    { "Luke", true },
+                    { "Han", false },
+                    { "Chewbacca", false }
+                };
+            characters.Remove("Han");  
+
+            foreach (KeyValuePair<string, bool> a in characters)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", a.Key, a.Value);
+            }
+
+
+
+
+        }
+    }
+}
+
